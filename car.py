ee44de1d06c1e9f223268c53404b844e69d1964b
alstit <alexyls78@gmail.com>
2019-12-07 21:11:47 +0100

    Add files via upload
    add pyvjoy to car.py to control the car

--- car.py
+++ car.py
@@ -12,11 +12,19 @@ import win32gui, win32ui, win32con, win32api
 import sys
 from datetime  import datetime
 
+def drive(direction,j):
+    inv_direction = direction
+    inv_direction = int((inv_direction)*16384+16384)
+   
+    print(inv_direction)
 
+    j.set_axis(pyvjoy.HID_USAGE_X, inv_direction)
 
 
 
 def main() :
+    j = pyvjoy.VJoyDevice(1)
+
 
     while(True):
         screen = ImageGrab.grab(bbox=(0,40,800,600))
@@ -27,8 +35,10 @@ def main() :
         files = {'myFile': stream.getvalue()}
 
         r= requests.post("http://localhost:3000/uploadfile",files=files)
-        print(r.content)
-
+        #print(type(r.content.decode('utf-8')))
+        #print(float(r.content.decode('utf-8')))
+        #print(r.content.decode('utf-8'))
+        drive(float(r.content.decode('utf-8')),j)
     
 if __name__ == "__main__":
     main()
